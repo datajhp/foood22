eeda8dc6da66086a3a53da1cb25151aa26157f1d
datajhp <jhp24228064@gmail.com>
2025-03-21 22:23:40 +0900

    Update 222.py

--- 222.py
+++ 222.py
@@ -34,7 +34,7 @@ with col1:
         f"""
      <div style="width: 650px; height: 700px; overflow: hidden;">
          <iframe src="{url3}" width="650" height="700" 
-                 style="transform: scale(0.5); transform-origin: 0 0;">
+                 style="transform: scale(1); transform-origin: 0 0;">
          </iframe>
      </div>
      """,
