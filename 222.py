6c96696f077d4038dbdb31cef6c782378f7f18b7
datajhp <jhp24228064@gmail.com>
2025-03-21 22:30:48 +0900

    Update 222.py

--- 222.py
+++ 222.py
@@ -32,7 +32,7 @@ with col1:
     st.subheader("📌 만나")
     st.markdown(
         f"""
-     <div style="width: 162.5px; height: 175px; overflow: hidden;">
+     <div style="width: 650px; height: 700px; overflow: hidden;">
          <iframe src="{url3}" width="650" height="700" 
                  style="transform: scale(0.5); transform-origin: 0 0;">
          </iframe>
