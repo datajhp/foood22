54922d6de92444bd85e579bf6737cf6c3aad9049
datajhp <jhp24228064@gmail.com>
2025-03-21 22:36:24 +0900

    Update 222.py

--- 222.py
+++ 222.py
@@ -33,7 +33,7 @@ with col1:
     st.markdown(
         f"""
      <div style="width: 650px; height: 700px; overflow: hidden;">
-         <iframe src="{url3}" width="650" height="6000" 
+         <iframe src="{url3}" width="650" height="3000" 
                  style="transform: scale(0.55); transform-origin: 0 0;">
          </iframe>
      </div>
