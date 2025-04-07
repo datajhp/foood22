06577270b0aa70b017944c85b0a81344a3f79d07
datajhp <jhp24228064@gmail.com>
2025-04-07 14:57:36 +0900

    Update 222.py

--- 222.py
+++ 222.py
@@ -82,7 +82,7 @@ with col11:
     st.title("🍽️오늘의 메뉴's🍽️")
 
 with col12:
-    components.html(html_code, height=500)
+    components.html(html_code, height=200)
 
 
 kst = pytz.timezone('Asia/Seoul')
