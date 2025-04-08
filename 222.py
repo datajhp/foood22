3fbf2aeb03cda429824bc82030dfd2c211d14e2f
datajhp <jhp24228064@gmail.com>
2025-04-08 11:14:41 +0900

    Update 222.py

--- 222.py
+++ 222.py
@@ -121,7 +121,7 @@ with col11:
     st.info(work_msg)
 
 with col12:
-    components.html(html_code, height=270)
+    components.html(html_code, height=250)
 
 # 카카오톡 채널 URL 2개
 url1 = "https://pf.kakao.com/_CiVis/posts"
